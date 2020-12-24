66b5ad08346545e6ec6d61ed00dae8d63bb7c4db
Johnathon Sullinger <scionwest@gmail.com>
2020-12-24 14:32:37 -0700

    Implemented the send method on cf-response

--- lib/lambda-events/cloudformation/cloud-formation-response.d.ts
+++ lib/lambda-events/cloudformation/cloud-formation-response.d.ts
@@ -3,4 +3,5 @@ export declare class CloudFormationResponse<TEventProperties> {
     event: CloudFormationEvent<TEventProperties>;
     context: any;
     constructor(event: CloudFormationEvent<TEventProperties>, context: any);
+    send<TOutput>(status: string, outputData: TOutput): Promise<void>;
 }
--- src/lambda-events/cloudformation/cloud-formation-response.ts
+++ src/lambda-events/cloudformation/cloud-formation-response.ts
@@ -1,4 +1,5 @@
-import { stat } from 'fs';
+import axios from 'axios';
+
 import { CloudFormationEvent } from './cloud-formation-event';
 
 export class CloudFormationResponse<TEventProperties> {
@@ -10,7 +11,28 @@ export class CloudFormationResponse<TEventProperties> {
         this.context = context;
     }
 
-    // async send<TOutput>(status: string, outputData: TOutput) {
+    async send<TOutput>(status: string, outputData: TOutput): Promise<void> {
+        const jsonResponse = JSON.stringify({
+            Status: status,
+            Reason: `See the details in CloudWatch LogStream: ${this.context.logStreamName}`,
+            PhysicalResourceId: this.context.logStreamName,
+            StackId: this.event.stackId,
+            RequestId: this.event.requestId,
+            LogicalResourceId: this.event.logicalResourceId,
+            Data: outputData
+        });
 
-    // }
+        console.log(`Response body:\n${jsonResponse}`);
+        const requestOptions = {
+            headers: { 'content-type': '', 'content-length': jsonResponse.length }
+        };
+
+        try {
+            await axios.put(this.event.responseUrl, jsonResponse, requestOptions);
+            console.log('Response to CloudFormation sent successfully');
+        } catch(err) {
+            console.log(err);
+            throw new Error('Unable to tell CloudFormation that the Lambda was completed.');
+        }
+    }
 }
