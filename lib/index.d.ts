478e694a384b99209c0065aaaea23085bafbc48c
Scionwest <scionwest+github@gmail.com>
2020-12-24 04:16:16 +0000

    added lib folder

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+export * from './api-gateway/api-gateway-event';
+export * from './api-gateway/api-gateway-request-context-identity';
+export * from './api-gateway/api-gateway-request-context';
+export * from './api-gateway/api-gateway-response';
